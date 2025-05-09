07ec28aadef11cb9591df4d7304a6d15e68ec4c1
NguyenHoang1122 <nguyenhoang9xbn@gmail.com>
2025-05-09 09:40:49 +0700

    doi-tien

---
+++ bai1/chuyen-doi-tien-te/usd-to-vnd/src/UsdToVnd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UsdToVnd {
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usd;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so tien USD :");
+        usd = scanner.nextDouble();
+        double quydoi = usd*vnd;
+        System.out.print("Gia tri VND : " + quydoi + " VND.");
+    }
+}
