18b8c0fc1c28c5cb7bd0767c6afd7cec993293a8
NguyenHoang1122 <nguyenhoang9xbn@gmail.com>
2025-05-12 09:02:07 +0700

    hien thi 20 so nguyen to

--- bai2/20-so-nguyen-to/prime-number/src/PrimeNumber.java
+++ bai2/20-so-nguyen-to/prime-number/src/PrimeNumber.java
@@ -6,6 +6,22 @@ public class PrimeNumber {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhap so luong so nguyen to can in ra :");
         numbers = scanner.nextInt();
-
+        int count = 0;
+        int number1 = 2;
+        while(count < numbers){
+            if(isPrimeNumber(number1)){
+                System.out.println(number1+" ");
+                count++;
+            }
+            number1++;
+        }
+        scanner.close();
+    }
+    public static boolean isPrimeNumber(int number){
+        if(number < 2) return false;
+        for(int i=2; i<= Math.sqrt(number); i++){
+            if(number % i == 0) return false;
+        }
+        return true;
     }
 }
