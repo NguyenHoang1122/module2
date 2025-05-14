517d60e168fdecacbbe66c247112578b2be985b1
NguyenHoang1122 <nguyenhoang9xbn@gmail.com>
2025-05-14 07:48:52 +0700

    xd-lop-hinh-chu-nhat

--- bai4/class-hinh-chu-nhat/create-rectangle-class/src/Rectangle.java
+++ bai4/class-hinh-chu-nhat/create-rectangle-class/src/Rectangle.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Rectangle {
+    double width,height;
+    public Rectangle(double width,double height){
+        this.width=width;
+        this.height=height;
+    }
+    public double getArea(){
+        return width*height;
+    }
+    public double getPerimeter(){
+        return (width+height)*2;
+    }
+    public String display(){
+        return "Rectangle{ width = "+width+", height = "+height+" }";
+    }
 }
