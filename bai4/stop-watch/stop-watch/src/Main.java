692158d410390f73b784c813668157145fd6ee05
NguyenHoang1122 <nguyenhoang9xbn@gmail.com>
2025-05-14 10:05:10 +0700

    stop watch

---
+++ bai4/stop-watch/stop-watch/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void selectionSort(int[] arr){
+        int n = arr.length;
+        for(int i = 0; i < n-1; i++){
+            int min = i;
+            for(int j = i+1; j < n; j++){
+                if(arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[100000];
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100000);
+        }
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        selectionSort(arr);
+        stopWatch.stop();
+        System.out.print("Thời gian thực thi (ms) là : " + stopWatch.getElapsedTime());
+    }
+}
